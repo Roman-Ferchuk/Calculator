5c13304ed343f71de65a7b57eee6f313afebddea
unknown <romanferchuk5@gmail.com>
2026-04-07 11:57:08 +0200

    some styles and declared functions

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function clearDisplay() {
+
+}
+
+function toggleSign() {
+
+}
+
+function appendPercent() {
+
+}
+
+function setOperator(operator) {
+
+}
+
+function appendNumber(number) {
+
+}
+
+function appendDecimal() {
+
+}
+
+function calculate() {
+    
+}
